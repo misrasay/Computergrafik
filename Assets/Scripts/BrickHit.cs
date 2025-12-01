35dd82964f0eaad9ed0d107d4506334bb90cf191
ssa9138 <ssa9138@students.fhv.at>
2025-12-01 18:04:10 +0100

    Animation versuch2

--- Assets/Scripts/BrickHit.cs
+++ Assets/Scripts/BrickHit.cs
@@ -5,31 +5,45 @@ using UnityEngine;
 
 public class BrickHit : MonoBehaviour
 {
-
     private BrickNumber brickNumber;
+    private Animator anim;
+
+    [Tooltip("Wie lange der Brick nach dem Hit noch existiert, damit die Animation ablaufen kann.")]
+    public float destroyDelay = 0.6f;
 
     void Awake()
     {
         brickNumber = GetComponent<BrickNumber>();
+        anim = GetComponent<Animator>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (!collision.gameObject.CompareTag("Ball"))
+            return;
 
-        if (collision.gameObject.CompareTag("Ball"))
+        int number = brickNumber.Number;
+        bool isCorrect = (number == EquationAnswer.currentAnswer);
+
+        if (anim != null)
         {
-
-            int number = brickNumber.Number;
-
-            bool isCorrect = (number == EquationAnswer.currentAnswer);
-
-            gameObject.SetActive(false);
-
-
-            GenerateEquation equation = FindObjectOfType<GenerateEquation>();
-            equation.OnBrickHit(isCorrect);
-
-            Destroy(gameObject);
+            if (isCorrect)
+                anim.SetTrigger("HitCorrect");
+            else
+                anim.SetTrigger("HitWrong");
         }
+
+        Collider col = GetComponent<Collider>();
+        if (col != null)
+            col.enabled = false;
+
+        GenerateEquation equation = FindObjectOfType<GenerateEquation>();
+        if (equation != null)
+        {
+            equation.OnBrickHit(isCorrect);
+        }
+
+
+        Destroy(gameObject, destroyDelay);
     }
 }
