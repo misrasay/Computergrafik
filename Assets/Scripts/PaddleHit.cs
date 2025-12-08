fb872e2c00b1ced6e2df299666c8ed12bcb12de7
mercury33 <vani.d@live.at>
2025-12-08 19:58:52 +0100

    added respawn text

--- Assets/Scripts/PaddleHit.cs
+++ Assets/Scripts/PaddleHit.cs
@@ -2,8 +2,33 @@ using UnityEngine;
 
 public class PaddleHit : MonoBehaviour
 {
+
+    [SerializeField] private GameObject hintText;
+    private float pulseScale = 0.1f;
+    private float pulseSpeed = 2f;
+
+    private float timeSinceLastHit = 0f;
+    private float timeToShow = 10f;
+
+
+    private void Start()
+    {
+
+        hintText.SetActive(false);
+        hintText.transform.localScale = Vector3.one;
+        
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
+
+        timeSinceLastHit = 0f;
+        if(hintText.activeSelf == true) {
+
+            hintText.SetActive(false);
+
+        }
+
         if (!collision.gameObject.CompareTag("Ball"))
             return;
 
@@ -17,5 +42,23 @@ public class PaddleHit : MonoBehaviour
         {
             brick.EnableAfterPaddleBounce();
         }
+    }
+
+    private void Update()
+    {
+
+        timeSinceLastHit += Time.deltaTime;
+
+        if (timeSinceLastHit >= timeToShow)
+        {
+
+
+            hintText.SetActive(true);
+
+            float pulse = (Mathf.Sin(Time.time * pulseSpeed) + 1f) * 0.5f;
+            float scale = 1f + pulse * pulseScale;
+            hintText.transform.localScale = Vector3.one * scale;
+        }
+
     }
 }
